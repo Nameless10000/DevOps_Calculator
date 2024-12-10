f0fce36cca80b16a6965df05d666809a0e73c8ea
Антонов Евгений Владимирович <evantonov@urfu.ru>
2024-12-10 15:38:18 +0500

    Prometheus

--- Program.cs
+++ Program.cs
@@ -1,6 +1,33 @@
+using OpenTelemetry.Metrics;
+using Prometheus;
+using static System.Net.WebRequestMethods;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(meterProviderBuilder =>
+    {
+        meterProviderBuilder.AddPrometheusExporter();
+
+        // Добавляем метрики ASP.NET Core
+        meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting",
+                                      "Microsoft.AspNetCore.Server.Kestrel");
+
+        // Добавляем метрику для HTTP-соединений
+        meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Http.Connections");
+
+        // Настраиваем сбор метрик длительности запросов
+        meterProviderBuilder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = [
+                    0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10
+                ]
+            });
+    });
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +42,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseRouting();
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapMetrics(); // Создает конечную точку для метрик на /metrics
+});
+
+app.MapPrometheusScrapingEndpoint();
 
 app.UseAuthorization();
 
